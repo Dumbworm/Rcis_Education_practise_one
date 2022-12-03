017cd178643514100ecb064fe778f6b7926f1b77
Dumbworm <116413284+Dumbworm@users.noreply.github.com>
2022-12-03 09:39:01 +0700

    Update rcis_ep_task_three.dart

--- rcis_ep_task_three/bin/rcis_ep_task_three.dart
+++ rcis_ep_task_three/bin/rcis_ep_task_three.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
-import 'dart:convert';
-import 'dart:async';
+// import 'dart:io';
+// import 'dart:convert';
+// import 'dart:async';
 
-void main() async {
+// void main() async {
 //   String nums = "";
 //   List<int> lots = [];
 //   int i = 1;
@@ -12,56 +12,55 @@ void main() async {
 //   int n = 0;
 //   Map<int, List<int>> tickets = {};
 //   File qwe = File('input (1).txt');
-//   Stream<String> lines = qwe.openRead()
-//     .transform(utf8.decoder)
-//     .transform(LineSplitter());
+//   Stream<String> lines =
+//       qwe.openRead().transform(utf8.decoder).transform(LineSplitter());
 //   int countLines = 0;
-//      await for (String line in lines) {
-//       if(countLines == 0){
-//         line += " ";
-//         for(int i = 0; i < line.length; ++i){
-//           if(line[i] != " "){
-//             nums += line[i];
-//           } else {
-//               winLot.add(int.parse(nums));
-//             nums = "";
-//             continue;
-//           }
+//   await for (String line in lines) {
+//     if (countLines == 0) {
+//       line += " ";
+//       for (int i = 0; i < line.length; ++i) {
+//         if (line[i] != " ") {
+//           nums += line[i];
+//         } else {
+//           winLot.add(int.parse(nums));
+//           nums = "";
+//           continue;
 //         }
 //       }
-//       if(countLines == 1){
-//         n = int.parse(line);
-//       }
-//       if(countLines >= 2){
-//         line += " ";
-//         lots = [];
-//         for(int j = 0; j < line.length; ++j){
-//           if(line[j] != " " ){
-//             nums += line[j];
-//           }
-//           if(line[j] == " " ){
-//               lots.add(int.parse(nums));
-//               nums = "";
-//           }
-//         }
-//         tickets.addAll({i : lots});
-//         i++;
-//       }
-//       ++countLines;
 //     }
-//   for(int j = 1; j <= n; ++j){
-//     for(int f = 0; f < tickets[j]!.length; ++f){
-//       for(int k = 0; k < winLot.length; ++k){
-//         if(tickets[j]![f] == winLot[k]){
+//     if (countLines == 1) {
+//       n = int.parse(line);
+//     }
+//     if (countLines >= 2) {
+//       line += " ";
+//       lots = [];
+//       for (int j = 0; j < line.length; ++j) {
+//         if (line[j] != " ") {
+//           nums += line[j];
+//         }
+//         if (line[j] == " ") {
+//           lots.add(int.parse(nums));
+//           nums = "";
+//         }
+//       }
+//       tickets.addAll({i: lots});
+//       i++;
+//     }
+//     ++countLines;
+//   }
+//   for (int j = 1; j <= n; ++j) {
+//     for (int f = 0; f < tickets[j]!.length; ++f) {
+//       for (int k = 0; k < winLot.length; ++k) {
+//         if (tickets[j]![f] == winLot[k]) {
 //           ++countWinLot;
 //         }
 //       }
-//       if(countWinLot >= 3){
+//       if (countWinLot >= 3) {
 //         resultTicket += "Lucky\n";
 //         break;
 //       }
 //     }
-//     if(countWinLot < 3){
+//     if (countWinLot < 3) {
 //       resultTicket += "Unlucky\n";
 //     }
 //     countWinLot = 0;
@@ -69,8 +68,13 @@ void main() async {
 //   File otv = File('output (1).txt');
 //   otv = await File('output (1).txt').writeAsString(resultTicket);
 // }
-// }                     (↑ zadanie 1 ↑)
+//                          (↑ zadanie 1 ↑)
 
+// import 'dart:io';
+// import 'dart:convert';
+// import 'dart:async';
+
+// void main() async {
 //   String nums = '';
 //   String otvet = '';
 //   File('nums.txt').readAsString().then((String a) {
@@ -91,16 +95,22 @@ void main() async {
 //           }
 //         }
 //       }
-//        File('nums.txt').writeAsString(otvet);
+//       File('nums.txt').writeAsString(otvet);
 //     }
 //   });
 // }
 // }               (↑ zadanie 2 ↑)
+// import 'dart:io';
+// import 'dart:convert';
+// import 'dart:async';
+
+// void main() async {
 //   var nums = '';
 //   var otvet = '';
 //   List<int> numbers = [];
-//    File('height.txt').readAsString().then((String a) {
-//      a+= ' ';
+//   try {
+//     File('height.txt').readAsString().then((String a) {
+//     a += ' ';
 //     for (int i = 0; i < a.length; i++) {
 //       for (int j = 0; j < a[i].length; j++) {
 //         if (a[i][j] != " ") {
@@ -109,25 +119,26 @@ void main() async {
 //         if (a[i][j] == " ") {
 //           try {
 //             int nums1 = int.parse(nums);
-//               otvet += "$nums ";
+//             otvet += "$nums ";
 //             nums = "";
 //             numbers.add(nums1);
 //           } catch (e) {
-//             print('это не число!');
+//             print("Isn't correct value");
 //             continue;
 //           }
 //         }
 //       }
 //     }
-//   zadacha(numbers);
-//    });
-//    }
-
+//     zadacha(numbers);
+//   });
+// } catch (e){
+//   print ('$e');
+// }
+// }
 // void zadacha(List<int> height) {
 //   int left = 0;
 //   int right = height.length - 1;
 //   int max = 0;
-
 //   while (left != right) {
 //     int distance = (left - right).abs();
 //     int otv = 0;
@@ -143,5 +154,5 @@ void main() async {
 //     }
 //   }
 //   print(max);
-// }          (↑ zadanie 3 ↑)
-}
+// }          
+//                              (↑ zadanie 3 ↑)
