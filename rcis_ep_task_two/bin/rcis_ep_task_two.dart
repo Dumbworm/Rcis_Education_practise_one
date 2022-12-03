ef224bee58ab7cf43d0301cd8509dd58e817a474
Dumbworm <116413284+Dumbworm@users.noreply.github.com>
2022-12-03 09:24:28 +0700

    Update rcis_ep_task_two.dart

--- rcis_ep_task_two/bin/rcis_ep_task_two.dart
+++ rcis_ep_task_two/bin/rcis_ep_task_two.dart
@@ -1,7 +1,7 @@
-import 'dart:io';
-import 'dart:math';
+// import 'dart:io';
+// import 'dart:math';
 
-void main() {
+// void main() {
 //   int a  = 150;
 //   List<int> mass =List.filled(100, 0);
 //   for(int i = 0;i < mass.length; i++){
@@ -9,8 +9,13 @@ void main() {
 //     a = a - 3;
 //   }
 //   print(mass);
+// }
 //          (↑ zadanie 1 ↑)
-//   int a = 1;
+//  import 'dart:io';
+// import 'dart:math';
+
+// void main() {
+// int a = 1;
 //   List<int> mass = List.filled(50, 0);
 //   for (int i = 1; i < mass.length; i++) {
 //     mass[0] = 1;
@@ -18,7 +23,12 @@ void main() {
 //     a = a + 2;
 //   }
 //   print(mass);
-//           (↑ zadanie 2 ↑)
+// }
+//          (↑ zadanie 2 ↑)
+// import 'dart:io';
+// import 'dart:math';
+
+// void main() {
 //   List<List<int>> temp = List.generate(
 //       12, (i) => List.generate((30), (j) => Random().nextInt(30) - 12));
 //   for (int i = 0; i < temp.length; i++) {
@@ -32,7 +42,6 @@ void main() {
 //   res.sort();
 //   print(res);
 // }
-
 // List<int> sred(List<List<int>> temp) {
 //   List<int> sred = [];
 //   for (int i = 0; i < temp.length; i++) {
@@ -44,8 +53,12 @@ void main() {
 //   }
 //   return sred;
 // }
-// }                    (↑ zadanie 4 ↑)
-//   stdout.write("Введите n: ");
+//          (↑ zadanie 4 ↑)
+// import 'dart:io';
+// import 'dart:math';
+
+// void main() {
+// stdout.write("Введите n: ");
 //   try {
 //     int n = int.parse(stdin.readLineSync()!);
 //     List nums = List.generate(n, (i) => List.generate(n, (j) => 0));
@@ -67,49 +80,53 @@ void main() {
 //     print("Incorrect value.");
 //   }
 // }
-//                (↑ zadanie 3 ↑)
-  Map map = {
-    'Январь': List.generate((30), (j) => Random().nextInt(5) - 35),
-    "Февраль": List.generate((30), (j) => Random().nextInt(5) - 30),
-    'Март': List.generate((30), (j) => Random().nextInt(10) - 5),
-    'Апрель': List.generate((30), (j) => Random().nextInt(10) + 5),
-    'Май': List.generate((30), (j) => Random().nextInt(10) + 5),
-    'Июнь': List.generate((30), (j) => Random().nextInt(20) + 15),
-    'Июль': List.generate((30), (j) => Random().nextInt(20) + 15),
-    'Август': List.generate((30), (j) => Random().nextInt(20) + 15),
-    'Сентябрь': List.generate((30), (j) => Random().nextInt(5) + 10),
-    'Октябрь': List.generate((30), (j) => Random().nextInt(5) + 10),
-    'Ноябрь': List.generate((30), (j) => Random().nextInt(5) + 10),
-    'Декабрь': List.generate((30), (j) => Random().nextInt(1) - 15)
-  };
-  sred(map);
-}
+//          (↑ zadanie 3 ↑)
+// import 'dart:io';
+// import 'dart:math';
 
-Map sred(map) {
-  var otvet = {
-    'Январь': 0,
-    "Февраль": 0,
-    'Март': 0,
-    'Апрель': 0,
-    'Май': 0,
-    'Июнь': 0,
-    'Июль': 0,
-    'Август': 0,
-    'Сентябрь': 0,
-    'Октябрь': 0,
-    'Ноябрь': 0,
-    'Декабрь': 0
-  };
-  for (var key in map.keys) {
-    double sum = 0;
-    for (int i = 0; i < 30; i++) {
-      sum += map[key][i];
-    }
-    otvet[key] = (sum ~/ 30);
-  }
-  for (var item in otvet.entries) {
-    print("${item.key} - ${item.value}");
-  }
-  return otvet;
-}
-//  (↑ zadanie 5 ↑)
+// void main() {
+//   Map map = {
+//     'Январь': List.generate((30), (j) => Random().nextInt(5) - 35),
+//     "Февраль": List.generate((30), (j) => Random().nextInt(5) - 30),
+//     'Март': List.generate((30), (j) => Random().nextInt(10) - 5),
+//     'Апрель': List.generate((30), (j) => Random().nextInt(10) + 5),
+//     'Май': List.generate((30), (j) => Random().nextInt(10) + 5),
+//     'Июнь': List.generate((30), (j) => Random().nextInt(20) + 15),
+//     'Июль': List.generate((30), (j) => Random().nextInt(20) + 15),
+//     'Август': List.generate((30), (j) => Random().nextInt(20) + 15),
+//     'Сентябрь': List.generate((30), (j) => Random().nextInt(5) + 10),
+//     'Октябрь': List.generate((30), (j) => Random().nextInt(5) + 10),
+//     'Ноябрь': List.generate((30), (j) => Random().nextInt(5) + 10),
+//     'Декабрь': List.generate((30), (j) => Random().nextInt(1) - 15)
+//   };
+//   sred(map);
+// }
+
+// Map sred(map) {
+//   var otvet = {
+//     'Январь': 0,
+//     "Февраль": 0,
+//     'Март': 0,
+//     'Апрель': 0,
+//     'Май': 0,
+//     'Июнь': 0,
+//     'Июль': 0,
+//     'Август': 0,
+//     'Сентябрь': 0,
+//     'Октябрь': 0,
+//     'Ноябрь': 0,
+//     'Декабрь': 0
+//   };
+//   for (var key in map.keys) {
+//     double sum = 0;
+//     for (int i = 0; i < 30; i++) {
+//       sum += map[key][i];
+//     }
+//     otvet[key] = (sum ~/ 30);
+//   }
+//   for (var item in otvet.entries) {
+//     print("${item.key} - ${item.value}");
+//   }
+//   return otvet;
+// }
+//          (↑ zadanie 5 ↑)
